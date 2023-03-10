395db913192b95fba6f9421729f4e2ec442b3980
ajeer333 <ajeer333@gmail.com>
2023-03-10 12:53:32 +0100

    commit

--- day-five/src/Components/GalleryComponent.jsx
+++ day-five/src/Components/GalleryComponent.jsx
@@ -1,82 +1,67 @@
 import { Component } from "react";
-import {Row, Container,Spinner,Alert} from 'react-bootstrap'
+import { Row, Container, Spinner, Alert } from "react-bootstrap";
 import SingleMovie from "./SingleMovie";
 
+class GalleryComponent extends Component {
+  state = {
+    movies: [],
+    isLoading: true,
+    isError: false,
+  };
 
+  fetchMovies = async () => {
+    try {
+      let res = await fetch(
+        "https://www.omdbapi.com/?apikey=2404898d&s=" + this.props.saga
+      );
+      if (res.ok) {
+        let data = await res.json();
 
-class GalleryComponent extends Component{
-
-    state = {
-        movies:[],
-        isLoading:true,
-        isError:false
+        this.setState({
+          movies: data,
+          isLoading: false,
+        });
+      } else {
+        this.setState({
+          isLoading: false,
+          isError: true,
+        });
+      }
+    } catch (error) {
+      this.setState({
+        isLoading: false,
+        isError: true,
+      });
     }
-   
-   
-    fetchMovies=async()=>{
-   
-        try{
-            let res=await fetch("https://www.omdbapi.com/?apikey=2404898d&s="+this.props.saga)
-            if(res.ok){
-                let data= await res.json()
-                
-    
-                this.setState({
-                    movies:data,
-                    isLoading:false,
-                  
-                })
-            }
-            else{
-                this.setState({
-                    isLoading:false,
-                    isError:true
-                })
-            }
-         
-        }
-        catch(error){
-            this.setState({
-                isLoading:false,
-                isError:true
-            })
-        }
-    }
-    componentDidMount(){
-        this.fetchMovies()
-    
-    }
-    
-    render(){
-      
-        return(
-      <Container>   
-        <h5 style={{color:"white"}}>{this.props.saga.toUpperCase()}</h5> 
-        {this.state.isError &&  <Alert variant="danger" className="w-50">Something's wrong</Alert>}
-      {this.state.isLoading && <div className="text-center"> <Spinner animation="border" variant="success" /></div>}
-   <Row className="mt-4 ">
- 
- 
+  };
+  componentDidMount() {
+    this.fetchMovies();
+  }
 
-
-   
-
-
-
-   {this.state.movies.Search !== undefined && ( this.state.movies.Search.map((m)=>
-
-            <SingleMovie movie={m} key={m.imdbID} />
-
-
-            )) }
-                          </Row>
-                          </Container>
-          
-        )
-    }
-
-
+  render() {
+    return (
+      <Container>
+        {/* {console.log()} */}
+        <h5 style={{ color: "white" }}>{this.props.saga.toUpperCase()}</h5>
+        {this.state.isError && (
+          <Alert variant="danger" className="w-50">
+            Something's wrong
+          </Alert>
+        )}
+        {this.state.isLoading && (
+          <div className="text-center">
+            {" "}
+            <Spinner animation="border" variant="success" />
+          </div>
+        )}
+        <Row className="mt-4 ">
+          {this.state.movies.Search !== undefined &&
+            this.state.movies.Search.map((m) => (
+              <SingleMovie movie={m} key={m.imdbID} />
+            ))}
+        </Row>
+      </Container>
+    );
+  }
 }
-export default GalleryComponent
-
-
+export default GalleryComponent;
